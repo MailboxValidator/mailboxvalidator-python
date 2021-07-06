383b910d379ffb2af4b106861aaede5401412c4f
MailboxValidator <support@mailboxvalidator.com>
2021-07-06 17:08:21 +0800

    Updated code to comply with Python code standard.

--- tests/test_webservice.py
+++ tests/test_webservice.py
@@ -5,12 +5,12 @@ import json
 
 import MailboxValidator
 
-def testinvalidapikey(global_data):
-    mbv = MailboxValidator.SingleValidation(global_data["apikey"])
+def test_invalid_api_key(global_data):
+    mbv = MailboxValidator.EmailValidation(global_data["apikey"])
     results = mbv.ValidateEmail('example@example.com')
     assert results['error_message'] == 'API key not found.'
 
-def testapikeyexist(global_data, capsys):
+def test_api_key_exist(global_data, capsys):
     if (global_data["apikey"] == 'YOUR_API_KEY'):
         with capsys.disabled():
             print ("You could enter a MailboxValidator API Key in tests/conftest.py for real web service calling test.")
@@ -19,36 +19,36 @@ def testapikeyexist(global_data, capsys):
     else:
         assert global_data["apikey"] != "YOUR_API_KEY"
 
-def testfunctionexist(global_data):
-    mbv = MailboxValidator.SingleValidation(global_data["apikey"])
+def test_function_exist(global_data):
+    mbv = MailboxValidator.EmailValidation(global_data["apikey"])
     errors = []
-    functions_list = ['ValidateEmail', 'DisposableEmail', 'FreeEmail']
+    functions_list = ['validate_email', 'is_disposable_email', 'is_free_email']
     for x in range(len(functions_list)): 
         # assert hasattr(mbv, functions_list[x]) == True, "Function did not exist."
-        if (hasattr(mbv, functions_list[x]) == False):
+        if (hasattr(mbv, functions_list[x]) is False):
             errors.append("Function " + functions_list[x] + " did not exist.")
     # assert no error message has been registered, else print messages
     assert not errors, "errors occured:\n{}".format("\n".join(errors))
 
-def testvalidateemail(global_data):
-    mbv = MailboxValidator.SingleValidation(global_data["apikey"])
-    results = mbv.ValidateEmail('example@example.com')
+def test_validate_email(global_data):
+    mbv = MailboxValidator.EmailValidation(global_data["apikey"])
+    results = mbv.validate_email('example@example.com')
     if (global_data["apikey"] == 'YOUR_API_KEY'):
         assert results['error_code'] == "101"
     else:
         assert results['status'] == "False"
 
-def testdisposableemail(global_data):
-    mbv = MailboxValidator.SingleValidation(global_data["apikey"])
-    results = mbv.DisposableEmail('example@example.com')
+def test_disposable_email(global_data):
+    mbv = MailboxValidator.EmailValidation(global_data["apikey"])
+    results = mbv.is_disposable_email('example@example.com')
     if (global_data["apikey"] == 'YOUR_API_KEY'):
         assert results['error_code'] == "101"
     else:
         assert results['is_disposable'] == "True"
 
-def testfreeemail(global_data):
-    mbv = MailboxValidator.SingleValidation(global_data["apikey"])
-    results = mbv.FreeEmail('example@example.com')
+def test_free_email(global_data):
+    mbv = MailboxValidator.EmailValidation(global_data["apikey"])
+    results = mbv.is_free_email('example@example.com')
     if (global_data["apikey"] == 'YOUR_API_KEY'):
         assert results['error_code'] == "101"
     else:
